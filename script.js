d07cad73cda281f9612b027932d4a3dacb03a5e7
Arthur <Debruit63@gmai.com>
2020-03-29 14:04:11 +0400

    fix: responsive layout

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const headerBtn = document.querySelectorAll(".nav_title");
 const gallery = document.querySelectorAll(".gallery_image");
 const radiobtn = document.querySelectorAll(".gallery_list");
 let items = document.querySelectorAll(".slider_blocks");
-
 let currentItem = 0;
 let isEnabled = true;
+
+// const galleryNav = document.querySelector(".gallery_ul");
 // Общая функция для хедера и галереи
-// =======
+// ==================================
 const chooseBtn = (element, style) => {
-  for (let i = 0; i < element.length; i++) {
-    element[i].addEventListener("click", function(event) {
+  for (let elem of element) {
+    elem.addEventListener("click", function(event) {
       element.forEach(el => {
         if (el.classList.contains(style)) el.classList.remove(style);
         if (!el.classList.contains(style)) {
@@ -25,45 +26,45 @@ const chooseBtn = (element, style) => {
 // =====
 chooseBtn(headerBtn, "activeList");
 
-for (let anchor of headerBtn) {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
+// for (let anchor of headerBtn) {
+//   anchor.addEventListener("click", function(e) {
+//     // e.preventDefault();
 
-    const block = anchor.getAttribute("href");
-    if (block === "home_section") {
-      document.body.scrollIntoView({
-        behavior: "smooth"
-      });
-    } else {
-      document.querySelector(`.${block}`).scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-      });
-    }
-  });
-}
-
-// document.addEventListener("scroll", anchor);
-// function anchor(event) {
-//   event.preventDefault();
-//   const curPos = window.scrollY;
-//   const sections = document.querySelectorAll(".section");
-//   const links = document.querySelectorAll(".nav_title");
-
-//   sections.forEach(elem => {
-//     console.log('тоже')
-//     if (elem.offsetTop <= curPos && elem.offsetTop + elem.offsetHeight > curPos) {
-//       links.forEach(a => {
-//         console.log('тоже работает')
-//         a.classList.remove("activeList");
-//         if (elem.getAttribute("id") === a.getAttribute("href")) {
-//           console.log('работает')
-//           a.classList.add("activeList");
-//         }
+//     const block = anchor.getAttribute("href").substr(1);
+//     if (block === "home_section") {
+//       document.body.scrollIntoView({
+//         behavior: "smooth"
+//       });
+//     } else {
+//       document.querySelector(`.${block}`).scrollIntoView({
+//         behavior: "smooth",
+//         block: "start"
 //       });
 //     }
 //   });
 // }
+
+document.addEventListener("scroll", anchor);
+function anchor(event) {
+  event.preventDefault();
+  const curPos = window.scrollY;
+  const sections = document.querySelectorAll(".section");
+  const links = document.querySelectorAll(".nav_title");
+
+  sections.forEach(elem => {
+    if (
+      elem.offsetTop <= curPos &&
+      elem.offsetTop + elem.offsetHeight > curPos
+    ) {
+      links.forEach(a => {
+        a.classList.remove("activeList");
+        if (elem.getAttribute("id") === a.getAttribute("href").substr(1)) {
+          a.classList.add("activeList");
+        }
+      });
+    }
+  });
+}
 //  слайдер
 // ========
 const isbackground = () => {
@@ -102,16 +103,16 @@ const showItem = direction => {
 };
 
 const previousItem = n => {
-  hideItem("to-left");
+  hideItem("to-right");
   changeCurrentItem(n - 1);
-  showItem("from-right");
+  showItem("from-left");
   isbackground();
 };
 
 const nextItem = n => {
-  hideItem("to-right");
+  hideItem("to-left");
   changeCurrentItem(n + 1);
-  showItem("from-left");
+  showItem("from-right");
   isbackground();
 };
 
@@ -172,76 +173,65 @@ phones.forEach = elem => {
 //   });
 // }
 
-// Радиобатн
-chooseBtn(radiobtn, "choosen");
-
-const reGallery = (targetStyle, imgStyle) => {
-  let target = document.querySelector("." + targetStyle);
-  let picture = document.querySelectorAll(".gallery_block");
-  if (target.classList.contains(targetStyle)) {
-    for (let i = 0; i < picture.length; i++) {
-      if (!picture[i].classList.contains(imgStyle)) {
-        picture[i].style.display = "none";
-      }
-      if (picture[i].classList.contains(imgStyle)) {
-        picture[i].style.display = "";
-      }
+// Блок с навигацией и галлерей
+// ============================
+for (let el of radiobtn) {
+  el.addEventListener("click", function() {
+    radiobtn.forEach(el => {
+      if (el.classList.contains("choosen")) el.classList.remove("choosen");
+      if (!el.classList.contains("choosen"))
+        event.target.classList.add("choosen");
+    });
+    if (el.classList.contains("choosen")) {
+      document.querySelectorAll(".gallery_block").forEach(elem => {
+        elem.style.order = Math.floor(
+          1 + Math.random() * document.querySelectorAll(".gallery_block").length
+        );
+      });
     }
-  }
-  if (target.classList.contains("all_btn")) {
-    for (let i = 0; i < picture.length; i++) {
-      picture[i].style.display = "";
-    }
-  }
-};
-
-document.querySelector(".web_btn").onclick = function() {
-  reGallery("web_btn", "web");
-};
-document.querySelector(".artwork_btn").onclick = function() {
-  reGallery("artwork_btn", "artwork");
-};
-document.querySelector(".graphic_btn").onclick = function() {
-  reGallery("graphic_btn", "graphic");
-};
-document.querySelector(".all_btn").onclick = function() {
-  reGallery("all_btn");
-};
-// Галерея
+  });
+}
 chooseBtn(gallery, "highlight");
 
 //Кнопка send
 // =========
-document.querySelector(".button").onclick = function auth(event) {
-  event.preventDefault();
-  let messageCard = document.createElement("div");
-  let messageAbout = document.querySelector(".subject_input").value;
-  let messageDescr = document.querySelector(".information_textarea").value;
-  let name = document.querySelector(".title_input").value;
-  let email = document.querySelector(".email_input").value;
-  if (isEnabled && name && email) {
-    messageCard.className = "message";
-    document.querySelector(".information_form").after(messageCard);
-    messageCard.innerHTML = `                           <h3 class="message_title">Письмо отправлено</h3>
+let messageCard = document.createElement("div");
+document
+  .querySelector(".information_form")
+  .addEventListener("submit", function auth(event) {
+    event.preventDefault();
+
+    let messageAbout = document.querySelector(".subject_input").value;
+    let messageDescr = document.querySelector(".information_textarea").value;
+    let name = document.querySelector(".title_input").value;
+    let email = document.querySelector(".email_input").value;
+    if (isEnabled && name && email) {
+      messageCard.className = "message";
+      document.querySelector(".information_form").after(messageCard);
+      messageCard.innerHTML = `                           <h3 class="message_title">Письмо отправлено</h3>
     <h5 class="message_theme">${
-      messageAbout === "" ? "Без темы" : "Тема: " + messageAbout
+      messageAbout === "" ? "Без темы" : `Тема: ${messageAbout}`
     }</h5>
     <h5 class="message_about">${
       messageDescr === ""
         ? "Без описания"
-        : "Описание: " + messageDescr.substr(0, 5)
+        : `Описание: " ${messageDescr.substr(0, 5)}`
     }</h5>
     <button class="message_button">OK</button>`;
-    isEnabled = false;
-    document.querySelector(".information_form").reset();
-  }
+      isEnabled = false;
+      document.querySelector(".information_form").reset();
+    }
+    document
+      .querySelector(".message_button")
+      .addEventListener("click", function removeMess() {
+        document.querySelector(".message").remove();
+        isEnabled = true;
+      });
 
-  document.querySelector(".message_button").onclick = function messOff() {
-    document.querySelector(".message").remove();
-    isEnabled = true;
-  };
-  setTimeout(function() {
-    messageCard.remove();
-    isEnabled = true;
-  }, 6000);
-};
+    setTimeout(function() {
+      isEnabled = true;
+      document.querySelector(".message").remove();
+    }, 5000);
+  });
+if (messageCard) {
+}
